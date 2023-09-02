8cd3ddd69d904cffb88a06173a9f885e1e54fa7f
malekmlzz <malekmlzz>
2023-09-02 23:39:58 +0330

    check restpassword

--- app/Http/Controllers/Auth/Restpassword/RestPasswordController.php
+++ app/Http/Controllers/Auth/Restpassword/RestPasswordController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Auth\Restpassword;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\User;
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Hash;
 use Kavenegar;
@@ -17,11 +16,17 @@ class RestPasswordController extends Controller
 {
     public function sendPasswordRestCode(Request $request)
     {
- 
-       $user = User::where('mobile', $request->phon_number)->first();
+        if ($request->national_code) {
+            $user = User::where('national_code', $request->national_code)->first();
+        } elseif ($request->lab_code) {
+            $user = User::where('lab_code', $request->lab_code)->first();
+        } else {
+            return response()->json([
+                'massege' => 'لطفا کد ملی یا کد ازمایشگاه وارد کنیید'
+            ]);
+        }
         if ($user) {
             $code = mt_rand(100000, 999999);
-            dd($code);
             try {
                 $sender = "1000630006300";        //This is the Sender number
                 $message = 'کد بازیابی رمز عبور شما در سامانه یکپارچه تشخیصی:' . $code;        //The body of SMS
@@ -38,15 +43,19 @@ class RestPasswordController extends Controller
                 echo $e->errorMessage();
             }
             Cache::put('password_rest_code:' . $user->id, $code, now()->addMinute(2));
+            return response()->json([
+                'data' => $user,
+                'massege' => 'کد یکبار مصرف با موفقیت ارسال شد'
+            ]);
         } else {
             return response()->json([
-                'massege' => 'حساب کاربری با این شماره تلفن موجود نیست'
+                'massege' => 'حساب کاربری موجود نیست'
             ]);
         }
     }
 
-    public function verifyPasswordRestCode(Request $request , $id)
-    {   
+    public function verifyPasswordRestCode(Request $request, $id)
+    {
         $user = User::find($id);
         $code = $request->code;
 
