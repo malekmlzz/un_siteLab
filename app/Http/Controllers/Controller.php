849ad935c452180c1d851d9eff3a26fcdad8c2bb
malekmlzz <malekmlzz>
2023-11-24 15:36:42 +0330

    zipfile

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -24,8 +24,9 @@ class Controller extends BaseController
         $zipFileName = random_int(1111, 9999) . 'sample.zip';
 
         $directoryPath = storage_path('app/public/experiments/' . $currentDate . $nationalCode . '/');
+        dd($directoryPath);
         if ($zip->open(public_path('experimentZip/'.$zipFileName), ZipArchive::CREATE) === TRUE) {
-           
+            
             $filesToZip = new FilesystemIterator($directoryPath);
             
             foreach ($filesToZip as $file) {
